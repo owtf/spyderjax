b3187d3d664d41793beede4931c6127788646be5
delta24 <viyat001@gmail.com>
2014-05-27 19:22:16 +0530

    browser module completed

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+## Note:  this requires the python bindings for selenium and pyphantomjs if you want to use it
 
 import sys
 import time
@@ -8,210 +9,158 @@ import string
 import re
 from urlparse import urlparse
 import getopt
+import lxml.html
+from lxml import etree
+
 from selenium.webdriver import *
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import *
 from selenium.webdriver.support.ui import WebDriverWait
 
-
 #***************************** BUILD your browser here************************************
-# Comment out the webdriver which will not be used
-'''
-profile = FirefoxProfile()
-profile.set_preference("network.proxy.type", 1)
-profile.set_preference("network.proxy.http", "127.0.0.1")
-profile.set_preference("network.proxy.http_port", "8008")
-profile.update_preferences()
-
-browser = Firefox(firefox_profile=profile)
-#driver = Chromedriver(<pathtoexecutable>)
-
-# OR could do this by defining it in a function
-
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-
-def get_profile():
-    # get the Firefox profile object
-    firefoxProfile = FirefoxProfile()
-    firefoxProfile.set_preference(<>)
-    return firefoxProfile
-
-driver = Firefox(firefox_profile=firefoxProfile)
-'''
-#******************************************************************************************
-
-# Default take no screenshot
-TakeScreenshots = False
+DRIVER = ["firefox", "chrome", "phantomjs"]
 
 
-def main():
-
-    patterns = []                   # Holds RegExp for in-scope domains
-    SitesToVisit = []               # URLs to visit
-    sitesAdded = 0                  # pages has been added since last run
-    numberOfSitesVisited = 0        # pages browsed
-
-    previousNumberOfSitesVisited = numberOfSitesVisited
-
-    #########################
-    # Parse command options #
-    #########################
-
-    # parse command line options
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hvl:sb:d:", ["help","screenshots","browser","domain"])
-    except getopt.error, msg:
-        print(msg)
-        print("for help use --help")
-        sys.exit(2)
-
-    # process options
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print(__doc__)
-            sys.exit(0)
-
-        if o in ("-b", "--browser"):
-            if a == "firefox":
-                browser = webdriver.Firefox() # Get local session of firefox
-            elif a == "chrome":
-                browser = webdriver.Chrome(executable_path="./chromedriver") # Get local session of chrome
-        else:
-            print("Please specify which browser you want to use")
-            sys.exit(0)
-
-        if o in ("-d", "--domain"):
-            addDomainPatterns(patterns, a)
-            print "Added %s to Domains in Scope" % a
-
-        if o in ("-s", "--screenshots"):
-            TakeScreenshots = True
-            print "Taking screenshots"
-
-    # process arguments
-    for arg in args:
-        if SitesToVisit.count(arg) < 1:
-            SitesToVisit.append(arg)
-            sitesAdded += 1
-
-    ##############################
-    # Go to each url in the list #
-    ##############################
-
-    for Site in SitesToVisit:
-        leftToVisit = len(SitesToVisit) - numberOfSitesVisited
-
-        print "=================================="
-        print "Have %s items in the list" % len(SitesToVisit)
-        print "Have visited %s sites" % numberOfSitesVisited
-        print "Added %s sites since last interation" % sitesAdded
-        print "Have %s sites left to visit" % leftToVisit
-        print "=================================="
-
-    #######################################
-    # Print out all the pages in the list #
-    #######################################
-        page = 0
-        for tmpSite in SitesToVisit:
-            page += 1
-            print "Page %d : %s" % (page, tmpSite)
-
-        ###############
-        # Go to a URL #
-        ###############
-        print "Surfing to  " + Site
-
-        browser.get(Site) # Load page
-
-        numberOfSitesVisited += 1
-        sitesAdded = 0
-
-        time.sleep(5) # Let the page load (wait 5 seconds)
-
-        #############################
-        # Get hold of all the links #
-        #############################
-
-        #links1 = browser.find_elements_by_xpath("//a[@href[contains(.,'http')]]")
-        links1 = browser.find_elements_by_xpath("//a[contains(@href,'http')]")
-        links2 = browser.find_elements_by_xpath("//div//a[contains(@href,'http')]")
-        links = links1 + links2
-
-        ##########################################
-        # Add all the links to the list of links #
-        ##########################################
-
-        for link in links:
-            try:
-                if link.is_displayed() and link.is_enabled():
-                # Add only links that are displayed and enabled
-                    newURL = link.get_attribute("href") # Get the URL
-
-                # Is the URL in scope?
-                if urlInScope(patterns, getDomain(newURL)):
-                    # Have we seen the URL before?
-                    if SitesToVisit.count(newURL) < 1:
-                    # Yes
-                        print "Added       " + newURL
-                        SitesToVisit.append(newURL)
-                        sitesAdded += 1
-                    else:
-                        # No
-                        print "Already had " + newURL
-
-            except Exception, e:
-                # Something went wrong (usually the DOM-tree is updated while we gather links)
-                print "Error: %s" % e
-
-        # Take a screenshot of the page
-        takeScreenshot(browser, str(numberOfSitesVisited) + ".png")
-
-        # No more links to visit, close and quit the browser
-        browser.close()
-        browser.quit()
-
-        print "Crawl done!"
-
-
-def addDomainPatterns(patterns, domain):
+class Browser(object):
     """
-    * Add regular expression patterns to a list of patterns
+    * This takes care of building a browser based on user preferences'and define its actions
+    * crawlwait defines the implicit time wait after firing an event
+    * crawlreload defines the tme wait after loading a URL
     """
-    patterns.append(re.compile("^.*\." + domain + "$"))
-    patterns.append(re.compile("^" + domain + "$"))
 
+    def __init__(self, driver, crawlwait=2, crawlreload=):
+        self.driver = driver   # Can be DRIVER[i]
+        self.crawlwait = crawlwait
+        self. crawlreload = crawlreload
 
-def urlInScope(patterns, url):
-    """
-    * Check if a given URL is in scope
-    """
-    for pattern in patterns:
-        if pattern.match(url):
-            print "In Scope:     " + url
-            return True
-        else:
-            print "Out of Scope: " + url
-            return False
+    def browserBuilder(self, driver):
+        """
+        * create a browser based on WebDriverWait
+        """
+        # handle each case
+        if driver == "firefox":
+            profile = FirefoxProfile()
+            profile.set_preference("network.proxy.type", 1)
+            profile.set_preference("network.proxy.http", "127.0.0.1")
+            profile.set_preference("network.proxy.http_port", "8008")
+            #use proxy for everything, including localhost
+            profile.setPreference("network.proxy.no_proxies_on", "");
+            profile.update_preferences()
+            browser = Firefox(firefox_profile=profile)
 
+            return browser
 
-def getDomain(url):
-    """
-    * Get the domain name from a URL
-    """
-    return urlparse(url)[1]
+        elif driver == "chrome":
+            options = ChromeOptions()
+            # set proxy options
+            optionsChrome.addArguments("--proxy-server=http://127.0.0.1:8008/")
+            browser = ChromeDriver(optionsChrome)
 
+            return browser
 
-def takeScreenshot(browser, filename):
-    """
-    * Take a screenshot of the page
-    """
-    if TakeScreenshots:
+        elif driver == "phantomjs":
+            #proxy configuration
+            service_args = [
+                            '--proxy=127.0.0.1:8008',
+                            '--proxy-type=http',
+            ]
+            browser = PhantomJS('../path_to/phantomjs', service_args=service_args)
+
+            return browser
+
+    def goToURL(self, url):
+        """
+        * For now, only valid URLs supported
+
+        # TODO: add exception checking for invalid URLs
+        """
+        try:
+            #navigate().to() and get() are synonyms :)
+            browser.get(url)
+            handlePopUps()
+        except WebDriverException, e:
+            pass   
+        else InterruptedException, e:
+            print "goToUrl got interrupted while waiting for the page to be loaded ", e
+            pass        
+    
+    def handlePopUps():
+        try:
+            # Execute JS
+            browser.execute_script("window.alert = function(msg){return true;};" \
+                    + "window.confirm = function(msg){return true;};" \
+                    + "window.prompt = function(msg){return true;};" \
+                )
+        except UnexpectedAlertPresentException, e:
+            print "Unexpected Alert element: ", e
+            pass
+
+    def fireEventWait():
+        """
+        * Fires the event and waits for a specified time. 
+        * webElement: is the element to fire event on.
+        * eventable: The HTML event type (onclick, onmouseover, ...).
+        * This will return true if firing event is successful.
+        * else throws an InterruptedException when interrupted during the wait.
+        """
+        try:
+            element.click()
+        except ElementNotVisibleException, e:
+            print "Element not present: ", e
+            pass
+
+    def screenshot(browser, file):
+        """
+        * Takes screenshot of current page
+        * Only some webdrivers support it
+        """
         try:
             print "Taking screenshot: %s" % filename
             browser.save_screenshot(filename)
         except Exception, e:
-            print "Error: %s" % e
+            print "Error: ", e
 
-#-----------------------------------------------------------------------------------
-if __name__ == "__main__":
-    main()
+    def goback():
+        """
+        * Gives the back navigation
+        * Not very accurate, could possibly result in error
+        """
+        try:
+            browser.back()
+        except:
+            pass
+
+    def close():
+        # * Closes the browser instance
+        print "Closing the browser instance..."
+        browser.quit()
+
+
+#******************************************************************************************
+class Eventable(object):
+    """
+    # Events common to elements given below
+    GLOBAL_EVENTS = ['onclick', 'ondblclick', 'onmousedown', 'onmousemove', 
+            'onmouseout', 'onmouseover', 'onmouseup']
+
+    # Special events for each element.
+    EVENTS_PER_ELEMENT = {
+            'body': ['onload'],
+            'form': ['onsubmit', 'onreset'],
+            'input' : ['onselect', 'onchange', 'onfocus', 'onblur','onkeydown', 'onkeypress', 'onkeyup'],
+            'textarea': ['onselect', 'onchange', 'onfocus', 'onblur', 'onkeydown', 'onkeypress', 'onkeyup'],
+            'select': ['onchange', 'onfocus', 'onblur'],
+            'button': ['onfocus', 'onblur'],
+            'label': ['onfocus', 'onblur']
+    }
+
+    * Eventable class: an element having an event attached to it (onclick, onmouseover, ...) so that it
+    * can change the DOM state.
+
+    * Right now, only support `click` event.
+    * The elements would be identified by the name, id, xpath.
+    """
+
+    def __init__(self, event, element):
+        self.event = 'click' # hardcoded for right now
+        self.element = element
